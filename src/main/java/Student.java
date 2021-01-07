c0f2455ce6d403b25eb60f1085596d8e7f87de98
lening rodas <lening.a.rodas@gmail.com>
2021-01-07 14:33:40 -0600

    part1 complete

---
+++ src/main/java/Student.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    public static long id;
+    public static String name;
+    public ArrayList<Integer> grades;
+
+
+
+
+    // returns the student's id
+    public long getId(){}
+
+    // returns the student's name
+    public String getName(){}
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){}
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){}
+
+    // returns the average of the students grades
+    public double getGradeAverage(){}
+}
